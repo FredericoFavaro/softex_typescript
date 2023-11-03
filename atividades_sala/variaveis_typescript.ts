9f7665fcf9f6de500b71d60491a07b3b35c5406f
fredericofavaro <fredericofavaro@gmail.com>
2023-11-02 23:58:43 -0300

    tipos de variaveis

--- atividades_sala/variaveis_typescript.ts
+++ atividades_sala/variaveis_typescript.ts
@@ -1,8 +1,26 @@
+// Crie uma variavel para cada tipo de dado
+
 let idade: number = 10;
+
 let vazio: null = null;
+
 let nome: string = "Frederico";
+
 let certo: boolean = true;
-let lista: (string|number)[] = [2,3,"casa"];
+
+let indefinido: undefined;
+
+// Arrays podem ser declarafas de diferentes formas:
+// Forma generica
+let lista1: Array<string> = ["casa", "carro", "moto"];
+// Com conchetes
+let lista2: string[] = ["casa", "carro", "moto"];
+// Definindo mais de um tipo de dado
+let lista3: Array<string|number> = ["casa", "carro", 8, "moto", 2, 5];
+let lista4: (string|number)[] = ["casa", "carro", 8, "moto", 2, 5];
+// Declarando como no javascript, mas assim se perde a tipagem
+let lista5 = ["casa", "carro", 8, "moto", 2, 5]
+
 let aluno: object = {nome: "Frederico", idade: 34};
 
 
