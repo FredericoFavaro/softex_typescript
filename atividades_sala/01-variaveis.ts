f7d76894eddb4cd22f3512f3e909abeb30503887
fredericofavaro <fredericofavaro@gmail.com>
2023-11-23 02:00:11 -0300

    organizando arquivos

--- atividades_sala/01-variaveis.ts
+++ atividades_sala/01-variaveis.ts
@@ -1,13 +1,16 @@
-// Crie uma variavel para cada tipo de dado
+/*
+Crie uma variavel para cada tipo de dado
+*/
 
+// Tipo numerico (number)
 let idade: number = 10;
-
+// Tipo vazio (null)
 let vazio: null = null;
-
+// Tipo String (string)
 let nome: string = "Frederico";
-
+// Tipo Boleano (boolean)
 let certo: boolean = true;
-
+//Tipo Indefinido (undefined)
 let indefinido: undefined;
 
 // Arrays podem ser declarafas de diferentes formas:
@@ -21,6 +24,7 @@ let lista4: (string|number)[] = ["casa", "carro", 8, "moto", 2, 5];
 // Declarando como no javascript, mas assim se perde a tipagem
 let lista5 = ["casa", "carro", 8, "moto", 2, 5]
 
+// Tipo Objeto (object)
 let aluno: object = {nome: "Frederico", idade: 34};
 
 
---
+++ atividades_sala/02-number.ts
@@ -0,0 +1,15 @@
+/*
+Metodos para o tipo de dado number:
+
+Math.abs():   Retorna o valor absoluto (positivo) de um numero.
+Math.round(): Arredonda um número para o inteiro mais proximo.
+Math.floor(): Arredonda um número para baixo para o inteiro mais proximo.
+Math.ceil():  Arredonda um número para cima para o inteiro mais proximo.
+Math.max():   Retorna o maior valor entre dois ou mais numeros.
+Math.min():   Retorna o menor valor entre dois ou mais numeros.
+*/
+
+let x:number = -5;
+
+console.log(x.toExponential());
+
